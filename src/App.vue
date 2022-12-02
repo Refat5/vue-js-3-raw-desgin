35597da5944c6bb2edf48cd8904b99d74ebd0629
refat5 <mdrefat382139@gmail.com>
2022-12-03 00:36:21 +0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorldVue />
+  <HelloOne/>
+  <HelloOne/>
+  <HelloOne/>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorldVue from './components/HelloWorld.vue';
+import HelloOne from'./components/heeloOne.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorldVue,
+    HelloOne
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/heeloOne.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h3>Hello Home Component </h3>
+        <h1>Hello Home Component </h1>
+
+    </div>
+</template>
+
+<script>
+export default{
+    name:'HelloOne',
+}
+
+</script>
+
+<style scoped>
+
+h2{
+    color:green;
+}
+h3{
+    color: gray;
+}
+</style>
